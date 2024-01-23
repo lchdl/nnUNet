f576f556ee9ccc34f285b7e0bf87c09bef9655f9
Chenghao Liu <chenghao1652@126.com>
2024-01-23 22:26:34 +0800

    Updates required pytorch version from "==1.7.0" to ">=1.7.0".

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(name='nnunet',
       author_email='f.isensee@dkfz-heidelberg.de',
       license='Apache License Version 2.0, January 2004',
       install_requires=[
-            "torch==1.7.0",
+            "torch>=1.7.0",
             "dicom2nifti",
             "scikit-image>=0.14",
             "medpy",
